858761bfb5f3a2239cfb3b91f33a8d387bade338
MarianoMJava <magri.3003@gmail.com>
2023-06-28 07:36:40 +0200

    Initial commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/Magri/Mariano/s04/t02/n01/S04T02N01MagriMariano/S04T02N01MagriMarianoApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Magri.Mariano.s04.t02.n01.S04T02N01MagriMariano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01MagriMarianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01MagriMarianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/Magri/Mariano/s04/t02/n01/S04T02N01MagriMariano/S04T02N01MagriMarianoApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Magri.Mariano.s04.t02.n01.S04T02N01MagriMariano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01MagriMarianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
